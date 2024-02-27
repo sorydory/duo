d1879df6b9f0d27abb981a9a3891a5d792f4774d
JeonSeongJin <ayclub8866@naver.com>
2024-02-27 17:32:48 +0900

    design: background, card design

--- src/app/page.js
+++ src/app/page.js
@@ -1,68 +1,159 @@
+"use client"; // TODO: 이후 Card와 분리 예정(Main:ServerSide , Card:ClientSide)
+
 import Image from "next/image";
+import { Fragment, useState } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
+
+const tier = [
+  { name: '티어 선택' },
+  { name: '아이언' },
+  { name: '브론즈' },
+  { name: '실버' },
+  { name: '골드' },
+  { name: '플레티넘' },
+  { name: '다이아몬드' },
+  { name: '마스터' },
+  { name: '그랜드마스터' },
+  { name: '챌린져' },
+]
 
 export default function Home() {
+  const [selected, setSelected] = useState(tier[0])
   return (
-    <div className="flex justify-center items-center h-dvh">
-      <div className="Main w-6/12 h-full p-4 bg-gray-100 rounded-lg">
-        <div className="Header mb-4 flex gap-3">
-        <button className="btn flex-1 border border-gray-400 rounded-lg">POSITION</button>
-          <button className="btn flex-1 border border-gray-400 rounded-lg">TIER</button>
-          <button className="btn flex-1 border border-gray-400 rounded-lg">TYPE</button>
-          <button className="btn flex-1 border border-gray-400 rounded-lg">POST</button>
+    
+    <div className="flex justify-center items-center h-dvh bg-sky-950">
+      <div className="Main w-6/12 h-full p-4 bg-sky-900 rounded-lg  min-w-[960px]">
+        <div className="Header mb-4 h-10 flex justify-between gap-3">
+        <div className="btn w-1/5 flex justify-center align-items">
+            <button className="Any bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-1 px-1 rounded-l">
+              <Image src="/images/Position_Top.png" width={30} height={30} alt="Position_Top"/>
+            </button>
+            <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-1 px-1 rounded-c">
+             <Image src="/images/Position_Jungle.png" width={30} height={30} alt="Position_Top"/>
+            </button>
+            <button className="Mid bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-1 px-1 rounded-c">
+              <Image src="/images/Position_Mid.png" width={30} height={30} alt="Position_Top"/>
+            </button>
+            <button className="AD bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-1 px-1 rounded-c">
+             <Image src="/images/Position_Bot.png" width={30} height={30} alt="Position_Top"/>
+            </button>
+            <button className="Sup bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-1 px-1 rounded-r">
+             <Image src="/images/Position_Support.png" width={30} height={30} alt="Position_Top"/>
+            </button>
+        </div>
+        <div className="w-1/6">
+      <Listbox value={selected} onChange={setSelected}>
+        <div className="relative mt-1">
+          <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+            <span className="block truncate">{selected.name}</span>
+            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+              <ChevronUpDownIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </span>
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+              {tier.map((person, personIdx) => (
+                <Listbox.Option
+                  key={personIdx}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                      active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                    }`
+                  }
+                  value={person}
+                >
+                  {({ selected }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? 'font-medium' : 'font-normal'
+                        }`}
+                      >
+                        {person.name}
+                      </span>
+                      {selected ? (
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
+          <div className="btn w-1/3 flex items-center justify-center">
+            <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l">솔로랭크</button>
+            <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-c">자유랭크</button>
+            <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r">칼바람나락</button>
+          </div>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">듀오 구하기</button>
         </div>
         <div className="Content">
-          <div className="Card border border-gray-400 p-4 h-96 rounded-lg">
-            <div className="Card-first h-1/6 flex justify-between bg-yellow-300">
-              <div className="Card-first-left justify-start bg-blue-300 flex items-center gap-3">
+          <div className="Card border border-gray-400 p-4 h-96 rounded-lg bg-sky-950">
+            <div className="Card-first h-1/6 flex justify-between">
+              <div className="Card-first-left justify-start flex items-center gap-3">
                 <div className="Card-first-left-left">
                   <Image src="/images/flower.png" width={48} height={48} alt="FLowerImage"/>
                 </div>
                 <div className="Card-first-left-right">
-                  <div className="Nickname">
+                  <div className="Nickname text-white">
                     닉네임
                   </div>
-                  <div className="LatestConnect">
+                  <div className="LatestConnect text-white">
                     최근 접속일 : 1일 전
                   </div>
                 </div>
               </div>
               <div className="Card-first-right justify-end flex items-center gap-3">
-                <div className="Playtype">
+                <div className="Playtype text-white">
                   솔로랭크
                 </div>      
                 <div><Image src="/images/mic.png"width={30} height={30} alt="MicImage" /></div>
-                <button className="detail border border-gray-400 rounded-lg">
+                <button className="detail border border-gray-400 rounded-lg text-white">
                   더보기
                 </button>
               </div>
             </div>
-            <div className="Card-second h-1/3 bg-green-300 flex items-center pl-3">
-              <div className="border border-gray-300">
+            <div className="Card-second h-1/3 flex items-center pl-3 border border-gray-300 ">
+              <div className="text-white">
                 내용
               </div>
             </div>
-            <div className="Card-third h-1/3 bg-pink-300 flex justify-around items-center">
-              <div className="Solorank flex flex-col">
-                <div className="h-1/5">솔로랭크</div>
+            <div className="Card-third h-1/3 flex justify-between items-center">
+              <div className="Solorank flex flex-col border border-gray-400 w-96 h-full">
+                <div className="h-1/5 text-white">솔로랭크</div>
                 <div className="h-4/5">
                   <Image src="/images/challenger.webp" width={64} height={64} alt="TierImage" />
                 </div>  
               </div>
-              <div className="Teamrank flex flex-col">
-              <div className="h-1/5">자유랭크</div>
+              <div className="Teamrank flex flex-col border border-gray-400 w-96 h-full">
+              <div className="h-1/5 text-white">자유랭크</div>
                 <div className="h-4/5">
                   <Image src="/images/challenger.webp" width={64} height={64} alt="TierImage" />
                 </div>  
                 </div>  
-              <div className="Mannerscore flex flex-col">
-              <div className="h-1/5">매너점수</div>
+              <div className="Mannerscore flex flex-col border border-gray-400 w-96 h-full">
+              <div className="h-1/5 text-white">매너점수</div>
                 <div className="h-4/5">
                   <Image src="/images/challenger.webp" width={64} height={64} alt="TierImage" />
                 </div>  
               </div>
               </div>
-            <div className="Card-fourth h-1/6 bg-violet-400 flex items-center pl-3">
-              아이콘
+            <div className="Card-fourth h-1/6 flex items-center pl-3 text-white border border-gray-300 ">
+              아이콘컴포넌트
             </div>
           </div>
         </div>
