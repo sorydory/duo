e8693979ca07efbe6be4bec221936b705a35e9fe
JeonSeongJin <ayclub8866@naver.com>
2024-02-29 17:02:26 +0900

    restructure: create server folder for spring boot server

---
+++ server/src/test/java/com/jiny/lolduo/LolduoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiny.lolduo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolduoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
