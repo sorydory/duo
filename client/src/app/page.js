da72bba27682090b3746922679d992dab385a438
Account Profile Fixer temp account <ejdbelle12@naver.com>
2024-03-08 13:29:05 +0900

    first commit

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,16 +1,27 @@
-"use client"
+"use client";
 
 // 필요한 라이브러리 및 컴포넌트 import
 import Image from "next/image";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import Card from "@/components/card/card";
 import Duoform from "@/components/modal/duoform";
 import { fetchData } from "@/components/card/data";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 // 큐 타입 및 티어 옵션 배열 정의
-const quetype = ['솔로랭크', '자유랭크', '칼바람나락', '일반게임'];
-const tiers = ['iron', 'bronze', 'silver', 'gold', 'platinum', 'emerald', 'diamond', 'master', 'grandmaster', 'challenger'];
+const quetype = ["솔로랭크", "자유랭크", "칼바람나락", "일반게임"];
+const tiers = [
+  "iron",
+  "bronze",
+  "silver",
+  "gold",
+  "platinum",
+  "emerald",
+  "diamond",
+  "master",
+  "grandmaster",
+  "challenger",
+];
 
 export default function Home() {
   // 상태 관리를 위한 useState 훅 사용
@@ -29,13 +40,12 @@ export default function Home() {
         const fetchedData = await fetchData();
         setCards(fetchedData);
       } catch (error) {
-        console.error('Failed to fetch data:', error);
+        console.error("Failed to fetch data:", error);
       }
     };
 
     // 데이터 초기 로드
     getData();
-
   }, []);
 
   // 필터링 기능을 위한 useEffect
@@ -53,61 +63,58 @@ export default function Home() {
         setVisibleCards(prev => prev + 5); // 5장씩 추가적으로 보이도록 설정
       }
     };
-    
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, [visibleCards]); // visibleCards가 변경될 때마다 스크롤 이벤트 감지
 
-// 큐 타입 필터링 함수
-const handleTypeFilter = (type) => {
-  // 이미 선택된 타입과 동일한 경우 필터링을 수행하지 않습니다.
-  if (type === selectedType) {
-    return;
-  }
+  // 큐 타입 필터링 함수
+  const handleTypeFilter = type => {
+    // 이미 선택된 타입과 동일한 경우 필터링을 수행하지 않습니다.
+    if (type === selectedType) {
+      return;
+    }
 
-  setSelectedType(type);
+    setSelectedType(type);
 
-  // DB에 저장된 큐 타입 값 확인 및 변경
-  let dbType;
-  switch (type) {
-    case '솔로랭크':
-      dbType = 'solo';
-      break;
-    case '자유랭크':
-      dbType = 'team';
-      break;
-    case '칼바람나락':
-      dbType = 'aram';
-      break;
-    case '일반게임':
-      dbType = 'normal';
-      break;
-    default:
-      dbType = null;
-  }
+    // DB에 저장된 큐 타입 값 확인 및 변경
+    let dbType;
+    switch (type) {
+      case "솔로랭크":
+        dbType = "solo";
+        break;
+      case "자유랭크":
+        dbType = "team";
+        break;
+      case "칼바람나락":
+        dbType = "aram";
+        break;
+      case "일반게임":
+        dbType = "normal";
+        break;
+      default:
+        dbType = null;
+    }
 
-  // 만약 선택된 큐 타입이 null이라면 모든 카드를 보여줍니다.
-  if (dbType === null) {
-    setFilteredCards(cards);
-    console.log('null임');
-  } else {
-    // DB에 저장된 큐 타입 값을 변환하여 필터링합니다.
-    const filteredCards = cards.filter(card => card.category === dbType);
-    setFilteredCards(filteredCards);
-  }
-};
-
-
-
+    // 만약 선택된 큐 타입이 null이라면 모든 카드를 보여줍니다.
+    if (dbType === null) {
+      setFilteredCards(cards);
+      console.log("null임");
+    } else {
+      // DB에 저장된 큐 타입 값을 변환하여 필터링합니다.
+      const filteredCards = cards.filter(card => card.category === dbType);
+      setFilteredCards(filteredCards);
+    }
+  };
 
   // 포지션 필터링 함수
-  const handlePositionFilter = (position) => {
+  const handlePositionFilter = position => {
     console.log(position);
     setSelectedPosition(position);
   };
 
   // 티어 필터링 함수
-  const handleTierFilter = (tier) => {
+  const handleTierFilter = tier => {
     setSelectedTier(tier);
   };
 
@@ -115,24 +122,24 @@ const handleTypeFilter = (type) => {
   const handleFilters = (type, position, tier) => {
     let filtered = cards;
 
-  // DB에 저장된 큐 타입 값 확인 및 변경
-  let dbType;
-  switch (type) {
-    case '솔로랭크':
-      dbType = 'solo';
-      break;
-    case '자유랭크':
-      dbType = 'team';
-      break;
-    case '칼바람나락':
-      dbType = 'aram';
-      break;
-    case '일반게임':
-      dbType = 'normal';
-      break;
-    default:
-      dbType = null;
-  }
+    // DB에 저장된 큐 타입 값 확인 및 변경
+    let dbType;
+    switch (type) {
+      case "솔로랭크":
+        dbType = "solo";
+        break;
+      case "자유랭크":
+        dbType = "team";
+        break;
+      case "칼바람나락":
+        dbType = "aram";
+        break;
+      case "일반게임":
+        dbType = "normal";
+        break;
+      default:
+        dbType = null;
+    }
 
     // 포지션 필터링
     if (position !== null) {
@@ -168,22 +175,40 @@ const handleTypeFilter = (type) => {
         <div className="Header mb-4 h-[40px] flex justify-between gap-3 top-0 z-10 m-4">
           {/* 포지션 필터링 버튼 */}
           <div className="btn w-1/6 flex justify-center align-items">
-            {['top', 'jungle', 'mid', 'ad', 'support'].map((position, index) => (
-              <button
-                key={index}
-                onClick={() => handlePositionFilter(position)}
-                className={`bg-slate-950 hover:bg-gray-400 text-gray-800 font-bold py-1 px-1 rounded-${index === 0 ? 'l' : index === 4 ? 'r' : 'c'} ${selectedPosition === position ? 'bg-gray-400' : 'bg-gray-800'}`}
-              >
-                <Image src={`/images/Position_${position}.png`} width={30} height={30} alt={`Position_${position}`} />
-              </button>
-            ))}
+            {["top", "jungle", "mid", "ad", "support"].map(
+              (position, index) => (
+                <button
+                  key={index}
+                  onClick={() => handlePositionFilter(position)}
+                  className={`bg-slate-950 hover:bg-gray-400 text-gray-800 font-bold py-1 px-1 rounded-${
+                    index === 0 ? "l" : index === 4 ? "r" : "c"
+                  } ${
+                    selectedPosition === position
+                      ? "bg-gray-400"
+                      : "bg-gray-800"
+                  }`}
+                >
+                  <Image
+                    src={`/images/Position_${position}.png`}
+                    width={30}
+                    height={30}
+                    alt={`Position_${position}`}
+                  />
+                </button>
+              )
+            )}
           </div>
           {/* 티어 선택 */}
           <div className="w-1/6 flex items-center">
-            <select className="text-sm w-64 h-8" onChange={(e) => handleTierFilter(e.target.value)}>
+            <select
+              className="text-sm w-64 h-8"
+              onChange={e => handleTierFilter(e.target.value)}
+            >
               <option value="">전체</option>
               {tiers.map((tier, index) => (
-                <option key={index} value={tier}>{tier}</option>
+                <option key={index} value={tier}>
+                  {tier}
+                </option>
               ))}
             </select>
           </div>
@@ -192,7 +217,7 @@ const handleTypeFilter = (type) => {
             <button
               onClick={() => handleTypeFilter(null)}
               className={`hover:text-yellow-600 text-xs text-white font-bold py-2 px-4 rounded-l ${
-                selectedType === null ? 'bg-gray-400' : 'bg-gray-800'
+                selectedType === null ? "bg-gray-400" : "bg-gray-800"
               }`}
             >
               전체
@@ -201,24 +226,40 @@ const handleTypeFilter = (type) => {
               <button
                 key={index}
                 onClick={() => handleTypeFilter(type)}
-                className={`hover:text-yellow-600 text-white text-xs font-bold py-2 px-4 rounded${index === 0 ? '-c' : index === quetype.length - 1 ? '-r' : '-c'} ${selectedType === type ? 'bg-gray-400' : 'bg-gray-800'}`}
+                className={`hover:text-yellow-600 text-white text-xs font-bold py-2 px-4 rounded${
+                  index === 0
+                    ? "-c"
+                    : index === quetype.length - 1
+                    ? "-r"
+                    : "-c"
+                } ${selectedType === type ? "bg-gray-400" : "bg-gray-800"}`}
               >
                 {type}
               </button>
             ))}
           </div>
           {/* 듀오 구하기 버튼 */}
-          <button className="bg-yellow-600 hover:bg-white hover:text-yellow-600 text-white font-bold py-2 px-4 rounded" onClick={openModal}>듀오 구하기</button>
+          <button
+            className="bg-yellow-600 hover:bg-white hover:text-yellow-600 text-white font-bold py-2 px-4 rounded"
+            onClick={openModal}
+          >
+            듀오 구하기
+          </button>
           <Duoform isOpen={isModalOpen} onClose={closeModal} />
         </div>
         {/* 컨텐츠 부분 */}
         <div className="Content p-4 space-y-4">
           <TransitionGroup>
-            {filteredCards !== null && filteredCards.slice(0, visibleCards).map((card, index) => (
-              <CSSTransition key={index} classNames="fade" timeout={500}>
-                <Card key={index} data={card} className={index === 0 ? 'mt-3' : ''} />
-              </CSSTransition>
-            ))}
+            {filteredCards !== null &&
+              filteredCards.slice(0, visibleCards).map((card, index) => (
+                <CSSTransition key={index} classNames="fade" timeout={500}>
+                  <Card
+                    key={index}
+                    data={card}
+                    className={index === 0 ? "mt-3" : ""}
+                  />
+                </CSSTransition>
+              ))}
           </TransitionGroup>
         </div>
       </div>
